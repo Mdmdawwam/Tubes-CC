86c711fa257b5ae919ef43e646f436ab6ded8477
Mdmdawwam <mdmdawwam@gmail.com>
2024-10-18 16:04:35 +0700

    #

---
+++ login.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBr2-ABQKJVHH9TQTnynMvebEzifxUzDhQ",
+  authDomain: "login-tubes-8b34c.firebaseapp.com",
+  projectId: "login-tubes-8b34c",
+  storageBucket: "login-tubes-8b34c.appspot.com",
+  messagingSenderId: "518077304231",
+  appId: "1:518077304231:web:4c47045ba6b3ce16c77a82",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
